ea2ca696e5ff17dc5ad277162775729cac660741
Steven Underwood <stevenunderwood2@hotmail.com>
2023-06-09 14:38:25 -0500

    Created files and folders, created rough html page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const db = require('./db')
+const PORT = process.env.PORT || 3001
+const AppRouter = require('./routes/AppRouter')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('I am Groot!')
+})
+app.use('/api', AppRouter)
+app.listen(PORT, () => {
+    console.log(`Express server on port: ${PORT}`)
+})
